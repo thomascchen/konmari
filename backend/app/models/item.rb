0ad5ada81f44130bcccfed223ed2a4936ca99f59
Thomas Chen <chen.thomas.c@gmail.com>
2015-07-31 20:55:01 -0400

    Migrate item table and add validation tests for item model

--- backend/app/models/item.rb
+++ backend/app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ActiveRecord::Base
+  validates :name, presence: true
+  validates :keep, presence: true, inclusion: { in: [true, false] }
 end
--- backend/db/migrate/20150801003717_create_items.rb
+++ backend/db/migrate/20150801003717_create_items.rb
@@ -1,8 +1,8 @@
 class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
-      t.string :name
-      t.boolean :keep
+      t.string :name, null: false
+      t.boolean :keep, null: false, default: false
 
       t.timestamps null: false
     end
