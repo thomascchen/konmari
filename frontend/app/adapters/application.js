a7a1ba51a9aa06e5577fd0592424af085efe1d20
Thomas Chen <chen.thomas.c@gmail.com>
2015-08-02 14:29:10 -0400

    Reinsert shouldReloadAll() to handle deprecation

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 export default DS.ActiveModelAdapter.extend({
   namespace: 'api/v1',
-  // shouldReloadAll() { return true; }
+  shouldReloadAll() { return true; }
 });
